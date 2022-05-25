ac8a3eac9b011c14c5c820eccff6bc0ae2db25cf
Artem <arstrel@gmail.com>
2022-05-24 19:13:19 -0700

    add custom mux

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ Learning objectives
 Routes:
 GET /greeting -> Hello world
 GET /customers -> list of customers in json by default of in xml if header "Content-Type: application/xml"
+GET /customers/{numeric value}
+POST /customers -> Post request received
--- app/app.go
+++ app/app.go
@@ -3,15 +3,22 @@ package app
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func Start() {
 	// define custom multiplexer. This is optional because we can use default mux
-	mux := http.NewServeMux()
+	// mux := http.NewServeMux()
+	router := mux.NewRouter()
 	// define routes
-	mux.HandleFunc("/greet", greet)
-	mux.HandleFunc("/customers", getAllCustomers)
+	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	// customer_id can only be numeric, 404 for all other cases
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
+
+	router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
 
 	// starting server. Second arg is a handler, if nil is passed - default handler will be used
-	log.Fatal(http.ListenAndServe(":8000", mux))
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
